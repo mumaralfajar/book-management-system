17c92e2eec31516a9b617fb91f6d502f7e738607
mumarualfajar <umar.fajar14@gmail.com>
2023-11-24 22:35:19 +0700

    mod ut

--- src/test/java/com/example/BookManagementSystemApplicationTests.java
+++ src/test/java/com/example/BookManagementSystemApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BookManagementSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        BookManagementSystemApplication.main(new String[] {});
+    }
 
 }
