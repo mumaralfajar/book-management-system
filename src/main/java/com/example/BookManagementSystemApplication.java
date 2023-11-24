6cb3e94b5c82b546a45cb4b59d877216dfc5ebf1
mumarualfajar <umar.fajar14@gmail.com>
2023-11-24 09:24:36 +0700

    change conf

--- src/main/java/com/example/BookManagementSystemApplication.java
+++ src/main/java/com/example/BookManagementSystemApplication.java
@@ -3,16 +3,10 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class BookManagementSystemApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
 
 		SpringApplication.run(BookManagementSystemApplication.class, args);
 	}
