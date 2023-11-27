7ea55ceb90861cd79cc72988bd0cd5b0779ff14b
mumarualfajar <umar.fajar14@gmail.com>
2023-11-27 07:42:13 +0700

    conf

--- src/test/java/com/example/BookManagementSystemApplicationTests.java
+++ src/test/java/com/example/BookManagementSystemApplicationTests.java
@@ -6,9 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BookManagementSystemApplicationTests {
 
-    @Test
-    public void contextLoads() {
-        BookManagementSystemApplication.main(new String[] {});
-    }
+	@Test
+	void contextLoads() {
+		BookManagementSystemApplication.main(new String[] {});
+	}
 
 }
--- src/test/java/com/example/model/BookTest.java
+++ src/test/java/com/example/model/BookTest.java
@@ -3,28 +3,45 @@ package com.example.model;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class BookTest {
+class BookTest {
 
     @Test
-public void testEqualsAndHashCode() {
-    Book book1 = new Book();
-    book1.setId(1L);
-    book1.setTitle("Test Title");
-    book1.setAuthor("Test Author");
-    book1.setIsbn("123456789");
+    void testGettersAndSetters() {
+        Book book = new Book();
+        book.setId(1L);
+        book.setTitle("Test Title");
+        book.setAuthor("Test Author");
+        book.setIsbn("123456789");
 
-    Book book2 = new Book();
-    book2.setId(1L);
-    book2.setTitle("Test Title");
-    book2.setAuthor("Test Author");
-    book2.setIsbn("123456789");
-
-    assertEquals(book1, book2);
-    assertEquals(book1.hashCode(), book2.hashCode());
-}
+        assertEquals(1L, book.getId());
+        assertEquals("Test Title", book.getTitle());
+        assertEquals("Test Author", book.getAuthor());
+        assertEquals("123456789", book.getIsbn());
+    }
 
     @Test
-    public void testToString() {
+    void testEqualsAndHashCode() {
+        Book book1 = new Book();
+        book1.setId(1L);
+        book1.setTitle("Test Title");
+        book1.setAuthor("Test Author");
+        book1.setIsbn("123456789");
+
+        Book book2 = new Book();
+        book2.setId(1L);
+        book2.setTitle("Test Title");
+        book2.setAuthor("Test Author");
+        book2.setIsbn("123456789");
+
+        assertEquals(book1, book2);
+        assertEquals(book1.hashCode(), book2.hashCode());
+
+        String expectedString = "Book(id=1, title=Test Title, author=Test Author, isbn=123456789)";
+        assertEquals(expectedString, book1.toString());
+    }
+
+    @Test
+    void testToString() {
         Book book = new Book();
         book.setId(1L);
         book.setTitle("Test Title");
